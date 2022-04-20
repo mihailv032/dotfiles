74231db4c0e21d007d53d4b51a5c8fd48e1ecf87
mihail <mishgun032@gmail.com>
2022-04-20 15:53:00 +0100

    changed the install script

--- install.sh
+++ install.sh
@@ -6,44 +6,49 @@ ussage() {
   echo 's system packages'
   echo 'f full package install'
   echo 'b base package install' 
-  echo 'c install all the config files'
-  echo 'w wine dependencies'
+  echo 'c install all the config files' echo 'w wine dependencies'
 }
 
 system() {
-  pacman -S sudo grub grub-btrfs efibootmgr dosfstools os-prober ntfs-3g mtools networkmanager \
+  pacman -S sudo grub efibootmgr dosfstools os-prober ntfs-3g mtools networkmanager \
   base-devel intel-ucode nvidia lib32-nvidia-utils nvidia-utils mesa mesa-demos xorg xmlto kmod \
   dkms inetutils bc libelf cpio perl tar xz
 }
 
 full() {
   sudo pacman -Syu
+  
+  system
+
   sudo pacman -S imagemagick xorg-xrandr deluge deluge-gtk xorg-xmodmap\
   steam ttf-liberation terminus-font ttf-dejavu gcolor2 picom code lutris wine-staging winetricks sddm xmonad openbox  \
-  rofi audacity figma-linux gimp gnome-system-monitor android-file-transfer nitrogen neofetch \
+  rofi audacity gimp gnome-system-monitor android-file-transfer nitrogen neofetch \
   nodejs node-gyp npm
+ 
   basePkg
+  
   wine
-  aur 
 }
 
 basePkg() {
-  sudo pacman -S xmonad alacritty xterm htop ranger thunar tumbler feh emacs gvim flameshot \
+  sudo pacman -S trayer xmobar alacritty xterm htop ranger thunar tumbler feh emacs gvim flameshot \
   ruby rust eom vlc pavucontrol pulseaudio lxappearance xfce4-settings chromium qutebrowser firefox \
   discord
-#  yay
-  # cargo install bat 
- # gem install colorls
+
+#  yayInstall
+	cargo install bat 
+	gem install colorls
 }
 
-yay() {
+yayInstall() {
   cd /tmp
-  git clone https://aur.archlinux.org/yay.git && cd yay	
+  git clone https://aur.archlinux.org/yay.git 
+  cd yay	
   makepkg -si
 }
 
 aur() {
-  yay -S scrot siji-git ttf-font-awesome polybar fluent-reader soundux rar gdu
+  yay -S scrot siji-git ttf-font-awesome polybar fluent-reader soundux rar gdu c-lolcat 
 }
 
 wine() {
@@ -64,44 +69,63 @@ vm() {
 config() {
 	cd $(dirname $(realpath $0))
 	dir=$(dirname $(realpath $0))
-#	ln -sf $dir/.bashrc ~/.bashrc
-#	ln -sf $dir/.vimrc ~/.vimrc
-#	ln -sf $dir/.bash_prompt ~/.bash_prompt
-#	ln -sf $dir/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
-#	ln -sf $dir/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
-#	ln -sf $dir/config/picom/picom.conf ~/.config/picom/picom.conf
-#	ln -sf $dir/config/neofetch/config.conf ~/.config/neofetch/config.conf
-#	ln -sf $dir/config/rofi/config.rasi ~/.config/rofi/config.rasi
-#	ln -sf $dir/config/xmobar/xb0 ~/.config/xmobar/xb0
-#	ln -sf $dir/config/xmobar/xmobarrc ~/.config/xmobar/xmobarrc
-#	ln -sf $dir/config/xmobar/xb3 ~/.config/xmobar/xb3
 
-	ln -nsf $dir/.xmonad/scr/ ~/.xmonad/scr/
-#	ln -nsf $dir/config/colorls ~/.config/colorls
-#	ln -nsf $dir/config/polybar ~/.config/polybar
-#	ln -nsf $dir/config/ranger ~/.config/ranger
-#	ln -nsf $dir/config/xmobar/scr ~/.config/xmobar/scr
-#	ln -nsf $dir/config/rofi/powermenu ~/.config/rofi/powermenu
-#	ln -nsf $dir/config/rofi/gameLauncher ~/.config/rofi/gameLauncher
-#	ln -nsf $dir/config/i3 ~/.config/i3
+	mkdir -p ~/.local/bin
+	sudo cp -r /usr/bin/lolcat ~/.local/bin/lolcat-c
+
+	mkdir -p ~/.emacs.d/
+	cp -r $dir/emacs-config/* ~/.emacs.d
+
+	mkdir -p ~/.config/alacritty
+	mkdir -p ~/.xmonad
+	mkdir -p ~/.config/picom
+	mkdir -p ~/.config/neofetch
+	mkdir -p ~/.config/rofi/powermenu
+	mkdir -p ~/.config/rofi/gameLauncher
+	mkdir -p ~/.config/xmobar
+	
+	ln -sf $dir/.bashrc ~/.bashrc
+	ln -sf $dir/.vimrc ~/.vimrc
+	ln -sf $dir/.bash_prompt ~/.bash_prompt
+	ln -sf $dir/.xmonad/xmonad.hs ~/.xmonad/xmonad.hs
+	ln -sf $dir/config/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
+	ln -sf $dir/config/picom/picom.conf ~/.config/picom/picom.conf
+	ln -sf $dir/config/neofetch/config.conf ~/.config/neofetch/config.conf
+	ln -sf $dir/config/rofi/config.rasi ~/.config/rofi/config.rasi
+	ln -sf $dir/config/xmobar/xb0 ~/.config/xmobar/xb0
+	ln -sf $dir/config/xmobar/xmobarrc ~/.config/xmobar/xmobarrc
+	ln -sf $dir/config/xmobar/xb3 ~/.config/xmobar/xb3
+
+	ln -nsf $dir/.xmonad/scr ~/.xmonad/
+	ln -nsf $dir/config/colorls ~/.config/colorls
+	ln -nsf $dir/config/polybar ~/.config/polybar
+	ln -nsf $dir/config/ranger ~/.config/ranger
+	ln -nsf $dir/config/xmobar/scr ~/.config/xmobar/scr
+	ln -nsf $dir/config/rofi/powermenu ~/.config/rofi/powermenu
+	ln -nsf $dir/config/rofi/gameLauncher ~/.config/rofi/gameLauncher
+	ln -nsf $dir/config/i3 ~/.config/i3
 		
   #setting default file manager 
-#  xdg-mime default thunar.desktop inode/directory
+  xdg-mime default thunar.desktop inode/directory
 
   #monitor settings for sddm
-#  sudo cp Xsetup /usr/share/sddm/scripts/Xsetup
+  sudo cp Xsetup /usr/share/sddm/scripts/Xsetup
 
   #monitor & keyboard settings 
 #  sudo cp xorg/* /etc/X11/xorg.conf.d/
+  sudo ln -nsf $dir/xorg/00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
+  sudo ln -nsf $dir/xorg/10-monitor.conf /etc/X11/xorg.conf.d/10-monitor.conf
 
   #config files 
-#  cp config/* $HOME/.config/
+#  cp -r config/* $HOME/.config/
 
 }
 
 case "$1" in 
+	a) aur ;;
 	s) system ;;
 	f) full ;;
+	y) yayInstall ;;
 	b) basePkg ;;
 	c) config ;;
 	w) wine ;;
